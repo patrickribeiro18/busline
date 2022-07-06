b0517e3dc0b98b4619fa0bb870e707a4ac503fd0
patrickribeiro18 <patrick.ribeiro39@gmail.com>
2022-07-06 13:44:19 -0300

    Update horarios.js

--- js/horarios.js
+++ js/horarios.js
@@ -67,8 +67,8 @@ function carregaHorarios(idViagemMigracao, date, nome){
 			if(tamanho > 0){
 				retorno = '<div class="ui fluid teal card">';
 				retorno += '<div class="content">';
-				retorno += '<div class="center aligned header">' + nome + '</div>';
-				retorno += '<div class="center aligned header">' + idViagemMigracao + '</div>';
+				retorno += '<div class="center aligned header">' +"Viagem: "+ nome + '</div>';
+				retorno += '<div class="center aligned header">' + "IdViagemMigração: "+ idViagemMigracao + '</div>';
 				retorno += '<div class="center aligned description">';
 				for (let index = 0; index < tamanho; index++) {
 					retorno += data[0].partidas[index] + " ";
@@ -78,7 +78,7 @@ function carregaHorarios(idViagemMigracao, date, nome){
 			}else{
 				retorno = '<div class="ui fluid olive card">';
 				retorno += '<div class="content"><div class="center aligned description">';
-				retorno += "<span style='color:#b5cc18'>Não há horários para a viagem " + nome + idViagemMigracao +  "</span>";
+				retorno += "<span style='color:#b5cc18'>Não há horários para a viagem " + nome +" "+ idViagemMigracao +  "</span>";
 				retorno += '</div></div></div>';
 				$('#detalhehorario').append(retorno);
 			}
